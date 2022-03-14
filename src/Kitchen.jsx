6d5262660492d7ab217171145b6bea62ebd413ae
awatersny <awatersny@gmail.com>
2022-03-14 17:24:40 -0400

    Change syntax

--- src/Kitchen.jsx
+++ src/Kitchen.jsx
@@ -4,11 +4,11 @@ import Sink from "./Sink";
 const Kitchen = (props) => {
   return ( 
     <div id={props.id} className="kitchen">
+        <h1 id="kitchen-label">Kitchen</h1>
       <div id='oven-sink'>
         <Oven id='oven' />
         <Sink id='sink' />
       </div>
-        <h1 id="kitchen-label">Kitchen</h1>
     </div>
   );
 }
